99a3814fce03aca9632f1493cead2c71fb0d7f70
Wyatt-Stanke <wyattstanke@icloud.com>
2023-01-27 14:25:39 +0000

    wot

--- process.ts
+++ process.ts
@@ -2,6 +2,6 @@ Deno.run({
   cmd: ["sudo", "apt-get", "install", "p7zip"],
 });
 Deno.run({
-  cmd: ["p7zip", "-d", "-f", "retroarch.7z"],
+  cmd: ["p7zip", "-d", "-f", "RetroArch.7z"],
 });
-await Deno.remove("retroarch.7z");
+await Deno.remove("RetroArch.7z");
